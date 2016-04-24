44c03e54c3347ff6b4dc4ea151067d64a2b3268c
Alex Sherbuck <Spactigus@gmail.com>
2016-04-23 23:42:49 -0400

    autocommit on change

--- linkedlist.js
+++ linkedlist.js
@@ -30,7 +30,7 @@
 				if (Math.abs(position) > this.length) {
 					console.log('Removal position greater than Linked List length');
 				} else  if (position < 0) {
-					console.log('Position must be greater than 0 in a singly linked list');
+					console.log('Position must be greater than 0 in a singly Linked List');
 				}
 
 					{
