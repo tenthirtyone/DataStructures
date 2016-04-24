9b77dd641da17bd03e312f7eb7e82dbd60b43b9f
Alex Sherbuck <Spactigus@gmail.com>
2016-04-23 23:45:40 -0400

    autocommit on change

--- linkedlist.js
+++ linkedlist.js
@@ -30,7 +30,10 @@
 					console.log('Position must be greater than 0 in a singly Linked List');
 				} else {
 					console.log('insert node at %s', position) {
-						
+						var temp = this. head;
+						while (position != 0) {
+
+						}
 					}
 				}
 			},
