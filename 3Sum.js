f65a62bab5d6c1bd462f2c7a1292053e08a28cd6
Alex Sherbuck <Spactigus@gmail.com>
2016-04-24 00:11:48 -0400

    autocommit on change

---
+++ 3Sum.js
@@ -0,0 +1,25 @@
+/* Given an array of integers find all pairs that sum to a target
+*/
+
+var intArr = [0,1,2,3,4,5];
+var target = 6;
+var results = [];
+
+// n^2
+for (var i = 0; i < intArr.length; i++) {
+	for (var j = i+1; j < intArr.length; j++) {
+		if (i + j === target) {
+			results.push([i,j]);
+		}
+	}
+}
+
+console.log(results);
+var results = [];
+var dict = {};
+
+// n
+for (var i = 0; i < intArr.length; i++) {
+	dict[intArr[i]] = intArr[i];
+}
+console.log(dict);
