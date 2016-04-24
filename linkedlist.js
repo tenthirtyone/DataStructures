9544b88106a43d2f81fa2d34dd67d91f59b3a711
Alex Sherbuck <Spactigus@gmail.com>
2016-04-23 23:39:24 -0400

    autocommit on change

--- linkedlist.js
+++ linkedlist.js
@@ -51,10 +51,11 @@
 	var ll = linkedList();
 	
 	ll.toString();
+	ll.addNode(1);
 	ll.addNode(2);
+	ll.addNode(3);
 	ll.addNode(4);
-	ll.addNode(6);
-	ll.addNode(9);
+	ll.addNode(5);
 	ll.toString();
 	ll.removeNode(2);
 }());
