75effed314d228ae03b158cb53b269ee9ca9b300
Alex Sherbuck <Spactigus@gmail.com>
2016-04-24 00:37:02 -0400

    autocommit on change

---
+++ LLTest.js
@@ -0,0 +1,10 @@
+var LinkedList = require('./linkedlist');
+var ll = LinkedList.LinkedList();
+
+ll.addNode(1);
+ll.addNode(2);
+ll.addNode(3);
+ll.addNode(4);
+ll.addNode(5);
+ll.insertNode(0, 9);
+ll.toString();
